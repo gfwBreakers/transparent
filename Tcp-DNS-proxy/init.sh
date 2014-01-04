271eaf88410bcf9ba4c09ebbc6ba281e82c39118
Leask Huang <i@leaskh.com>
2014-01-05 01:45:22 +0800

    Freezed @ Sun Jan  5 01:45:22 CST 2014

--- Tcp-DNS-proxy/init.sh
+++ Tcp-DNS-proxy/init.sh
@@ -2,6 +2,6 @@
 sudo apt-get install libevent-dev
 sudo pip install gevent
 sudo pip install dnspython
-sudo ln -sf /gfwbreakers/transparent/tcp-dns-proxy/etc/init.d/tcp-dns-proxy /etc/init.d/
+sudo ln -sf /gfwbreakers/transparent/Tcp-DNS-proxy/etc/init.d/tcp-dns-proxy /etc/init.d/
 sudo update-rc.d tcp-dns-proxy defaults
 sudo service tcp-dns-proxy restart
