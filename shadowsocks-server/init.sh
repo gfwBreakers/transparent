2a64a32f2d526b687c1b0591e5cd949c6afe560d
Leask Huang <i@leaskh.com>
2014-06-18 01:20:49 +0800

    upgraded shadow to python version

--- shadowsocks-server/init.sh
+++ shadowsocks-server/init.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 sudo apt-get install python-pip python-m2crypto
 sudo pip install shadowsocks
-
-
-
-sudo npm install -g shadowsocks
-sudo ln -sf /gfwbreakers/transparent/shadowsocks-server/etc/init.d/shadowsocks-server /etc/init.d/
-sudo update-rc.d shadowsocks-server defaults
-sudo service shadowsocks-server restart
+# sudo echo 'ulimit -n 51200' >> /etc/default/supervisor
+sudo ln -sf /gfwbreakers/transparent/shadowsocks-server/etc/supervisor/conf.d/shadowsocks.conf /etc/shadowsocks.json
+service supervisor start
+supervisorctl reload
