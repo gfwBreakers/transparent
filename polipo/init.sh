3805d23301a801d52d07cb94cf60a7aea4e06847
Leask Huang <i@leaskh.com>
2014-06-18 00:58:18 +0800

    removed polipo configs

--- polipo/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sudo mkdir /gfwbreakers/transparent/.polipo-cache
-sudo apt-get install polipo
-sudo ln -sf /gfwbreakers/transparent/polipo/etc/polipo/config /etc/polipo/
-sudo service polipo restart
--- shadowsocks-server/init.sh
+++ shadowsocks-server/init.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+sudo apt-get install python-pip python-m2crypto
+sudo pip install shadowsocks
+
+
+
 sudo npm install -g shadowsocks
 sudo ln -sf /gfwbreakers/transparent/shadowsocks-server/etc/init.d/shadowsocks-server /etc/init.d/
 sudo update-rc.d shadowsocks-server defaults
